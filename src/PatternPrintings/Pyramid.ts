a8a03fd57a042e2abae07e0a3422c90c03e70739
chohan <chohankoti333@gmail.com>
2025-03-04 22:10:04 +0530

    04/03/25-10:10PM

--- src/PatternPrintings/Pyramid.ts
+++ src/PatternPrintings/Pyramid.ts
@@ -31,27 +31,51 @@ const printPyramidPattern = ({
           console.log();
           break;
         }
+        case PyramidType.Alphabets: {
+          for (let i = 1; i <= rows; i++) {
+            console.log(
+              Array.from({ length: i }, (_, j) =>
+                String.fromCharCode(65 + j)
+              ).join(" ")
+            );
+          }
+          console.log();
+          for (let i = 1; i <= rows; i++) {
+            console.log(
+              Array.from({ length: i }, () =>
+                String.fromCharCode(65 + i - 1)
+              ).join(" ")
+            );
+          }
+          console.log();
+          break;
+        }
       }
       break;
     }
     case Angle.PyramidInverted: {
-        switch (type) {
-            case PyramidType.Stars: {
-            for(let i = rows; i >= 1; i--){
-                console.log(" ".repeat(rows - i) + "* ".repeat(i));
-            }
-            console.log();
-            break;
-            }
+      switch (type) {
+        case PyramidType.Stars: {
+          for (let i = rows; i >= 1; i--) {
+            console.log(" ".repeat(rows - i) + "* ".repeat(i));
+          }
+          console.log();
+          break;
         }
-        break;
-        }
-    default: {  
-        console.log("Invalid angle type");
-        break;
+      }
+      break;
+    }
+    default: {
+      console.log("Invalid angle type");
+      break;
     }
   }
 };
 
-printPyramidPattern({ rows: 5, type: PyramidType.Stars, angle: Angle.Pyramid });
-printPyramidPattern({ rows: 5, type: PyramidType.Stars, angle: Angle.PyramidInverted });
+// printPyramidPattern({ rows: 5, type: PyramidType.Stars, angle: Angle.Pyramid });
+// printPyramidPattern({ rows: 5, type: PyramidType.Stars, angle: Angle.PyramidInverted });
+printPyramidPattern({
+  rows: 5,
+  type: PyramidType.Alphabets,
+  angle: Angle.Pyramid,
+});
